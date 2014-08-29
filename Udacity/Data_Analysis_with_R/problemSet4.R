caff29b8cad1b80cd945d4922c0a497040e950e2
Damian Naylor <damian.naylor@btopenworld.com>
2014-08-29 17:15:01 +0100

    almost finished problem set 4

--- Udacity/Data_Analysis_with_R/problemSet4.R
+++ Udacity/Data_Analysis_with_R/problemSet4.R
@@ -67,6 +67,43 @@ ggplot(data = subset(diamonds, diamonds$volume<800), aes(x = volume, y = price))
 
 #Q12
 #detach("package:plyr", unload=TRUE)
-install.packages(dplyr)
+#install.packages('dplyr')
 library(dplyr)
+clarityGroup <- group_by(diamonds, clarity)
+diamondsByClarity <- summarise(clarityGroup,
+                        mean_price = mean(price),
+                        median_price = median(price),
+                        min_price = min(price),
+                        max_price = max(price),
+                        n = n())
 
+#Q13
+diamonds_by_clarity <- group_by(diamonds, clarity)
+diamonds_mp_by_clarity <- summarise(diamonds_by_clarity, mean_price = mean(price))
+diamonds_by_color <- group_by(diamonds, color)
+diamonds_mp_by_color <- summarise(diamonds_by_color, mean_price = mean(price))
+
+clarityGraph <- ggplot(data=diamonds_mp_by_clarity, aes(x=clarity, y=mean_price)) +
+                  geom_bar(stat="identity")
+colorGraph <- ggplot(data=diamonds_mp_by_color, aes(x=color, y=mean_price)) +
+                geom_bar(stat="identity")
+
+#install.packages('gridExtra')
+library(gridExtra)
+grid.arrange(clarityGraph, colorGraph)
+
+#Q14
+?diamonds
+#color and clarity seem to be inversely mean price which is the opposite of what you'd expect
+diamonds_by_cut <- group_by(diamonds, cut)
+diamonds_mp_by_cut <- summarise(diamonds_by_cut, mean_price = mean(price))
+ggplot(data=diamonds_mp_by_cut, aes(x=cut, y=mean_price)) +
+  geom_bar(stat="identity")
+#same with cut
+
+#Q15
+#Continuation of EDA on a dataset of the student's choice from gapminder.org from problem set 3
+
+
+
+#Q15
