2ed22549bfc306cc0a9000af1ae580ae980449d9
Damian Naylor <damian.naylor@btopenworld.com>
2014-08-29 23:10:47 +0100

    added Peter Norvig Python Udacity course

---
+++ Design_of_Computer_Programs__Udacity/lessonsProblemSets.py
@@ -0,0 +1,19 @@
+# Design of Computer Programs - Udacity
+
+## LESSON 1 ##
+
+# Problem Set
+#Q1
+def best_hand(hand):
+	return max(itertools.combinations(hand, 5), key=hand_rank)
+	
+#Q2
+
+
+
+## LESSON 2 ##
+
+# Problem Set
+#Q1
+
+	
